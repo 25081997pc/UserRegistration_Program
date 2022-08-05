93f843dc3955b8a739f9852d1a0a5914c14c5de2
25081997pc <104768537+25081997pc@users.noreply.github.com>
2022-08-05 17:22:04 +0530

    Update UserRegistration.cs

--- UserRegistrationProgram/UserRegistration.cs
+++ UserRegistrationProgram/UserRegistration.cs
@@ -11,7 +11,7 @@ namespace UserRegistrationProgram
     {
         public string regex_FirstName = "^[A-Z]{1}[a-z]{2,8}$";
         public string regex_LastName = "^[A-Z]{1}[a-z]{2,8}$";
-        public string regex_EmailID = "^[a-z0-9]{1,10}[.,+,-]*[a-z0-9]{1,10}[@]{1}[a-z0-9]{1,9}[.]{1}[a-z]{2,5}[.]*[a-z]{1,3}$";
+        public string regex_EmailID = "^[a-zA-Z0-9]+([.,+,-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,})?$";
         public string regex_MobileNO = "^[+]*[9]{1}[1]{1}[' ']*[0-9]{10}$";
         public string regex_Password = "^[A-Z]{1}[a-zA-Z0-9!@#$%^&]{7}$";
 
